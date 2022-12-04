84f16d2309a641306ae10e02eeab47a053c33a5c
lijing <jingli14@tsinghua.org.cn>
2022-12-04 10:48:59 +0800

    init ant-design with out customTheme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## this is a template for React+ts
 
-tag 0.1 : craco + eslint + prettier
+### main分支
+#### 1. tag 0.1 craco + eslint + prettier
+- 最基础的配置 
+- 使用craco进行webpack自定义配置
+- 使用eslint+prettier进行代码格式化规范
+
+#### 2. tag router+redux+styled-components
+- 添加了路由和redux
+- 使用cssinjs方案styled-components
+- 规范了每个组件的构建，包含index.tsx和style.ts两个文件
+
+#### 3. tag axios+ts
+- 对axios进行封装
+
+### antdesign
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from '@/router'
 import { useNavigate } from 'react-router-dom'
+import { Button } from 'antd'
 
 function App() {
   const navigate = useNavigate()
@@ -13,6 +14,7 @@ function App() {
       <button onClick={() => handleClick('/login')}>Login</button>
       <button onClick={() => handleClick('/home')}>Home</button>
       <Suspense fallback="">{useRoutes(routes)}</Suspense>
+      <Button type="primary">button</Button>
     </div>
   )
 }
